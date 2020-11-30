a9b29facf9540bd1fce7746172daa3ba1b252187
José Andrés Herrera Alcantar <andres25b.ha@hotmail.com>
2020-11-30 17:44:39 -0600

    Cambio de tipo de dato
    Se modifico el tipo de dato del id usuario entity

--- microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/entity/Alumno.java
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/entity/Alumno.java
@@ -19,7 +19,7 @@ public class Alumno implements Serializable {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer id;
+	private Long id;
 
 	private String nombre;
 
@@ -36,11 +36,11 @@ public class Alumno implements Serializable {
 		this.createAt = new Date();
 	}
 
-	public Integer getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(Integer id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 
